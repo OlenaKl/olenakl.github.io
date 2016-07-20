8848c48dceda9a4f99a4c44debaa94f697e91d60
Elena.Kulichenko <elena.kulichenko@devellar.com>
2016-07-20 10:24:10 +0300

    trying the github io

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 /**
  * Created by Elena.Kulichenko on 19.07.2016.
  */
+$(document).ready(function(){
+    $.ajax({
+        url: 'http://openexchangerates.org/latest.json',
+        dataType: 'jsonp',
+        success: function(json) {
+            // Rates are in `json.rates`
+            // Base currency (USD) is `json.base`
+            // UNIX Timestamp when rates were collected is in `json.timestamp`
 
-$.getJSON(
-    'https://www.instagram.com/justinbieber/media/',
-    function(data) {
-    console.log(data);
-    }
-);
+            rates = json.rates;
+            base = json.base;
+            console.log(rates);
+        }
+    });
+});
