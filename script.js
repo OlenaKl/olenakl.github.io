04648e365d0b66e2751eec4a7349e520bfc45def
Elena.Kulichenko <elena.kulichenko@devellar.com>
2016-07-20 13:30:51 +0300

    trying the github io

--- script.js
+++ script.js
@@ -2,15 +2,41 @@
  * Created by Elena.Kulichenko on 19.07.2016.
  */
 $(document).ready(function(){
-    // OR using $.ajax()
-    $.ajax({
-        type:     "GET",
-        url:      "https://spreadsheets.google.com/feeds/list/o13394135408524254648.240766968415752635/od6/public/basic?alt=json-in-script&callback=someFunction",
-        dataType: "jsonp",
-        success: function(data){
-            console.log(data);
-        }
-    });
+    // Accepts a url and a callback function to run.
+    function requestCrossDomain( site, callback ) {
 
+        // If no url was passed, exit.
+        if ( !site ) {
+            alert('No site was passed.');
+            return false;
+        }
+
+        // Take the provided url, and add it to a YQL query. Make sure you encode it!
+        var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=cbFunc';
+
+        // Request that YSQL string, and run a callback function.
+        // Pass a defined function to prevent cache-busting.
+        $.getJSON( yql, cbFunc );
+
+        function cbFunc(data) {
+            // If we have something to work with...
+            if ( data.results[0] ) {
+                // Strip out all script tags, for security reasons.
+                // BE VERY CAREFUL. This helps, but we should do more.
+                data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
+
+                // If the user passed a callback, and it
+                // is a function, call it, and send through the data var.
+                if ( typeof callback === 'function') {
+                    callback(data);
+                }
+            }
+            // Else, Maybe we requested a site that doesn't exist, and nothing returned.
+            else throw new Error('Nothing returned from getJSON.');
+        }
+    }
+    requestCrossDomain('https://www.instagram.com/justinbieber/media/', function(results) {
+        console.log(results)
+    });
 
 });
