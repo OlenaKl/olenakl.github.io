13e3d5033b6731c7fa3bc465a361a1755fa51022
Elena.Kulichenko <elena.kulichenko@devellar.com>
2016-07-20 09:55:21 +0300

    trying the github io

--- script.js
+++ script.js
@@ -2,26 +2,28 @@
  * Created by Elena.Kulichenko on 19.07.2016.
  */
 
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-        var status = xhr.status;
-        if (status == 200) {
-            callback(null, xhr.response);
-        } else {
-            callback(status);
-        }
-    };
-    xhr.send();
+var getJSON = function(url) {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+            var status = xhr.status;
+            if (status == 200) {
+                resolve(xhr.response);
+            } else {
+                reject(status);
+            }
+        };
+        xhr.send();
+    });
 };
 
-getJSON('https://www.instagram.com/justinbieber/media', function(err, data) {
-    if (err != null) {
-        alert('Something went wrong: ' + err);
-    } else {
-        alert('Your Json result is:  ' + data.result);
-        result.innerText = data.result;
-    }
+getJSON('https://www.googleapis.com/freebase/v1/text/en/bob_dylan').then(function(data) {
+    alert('Your Json result is:  ' + data.result); //you can comment this, i used it to debug
+
+    var a = data.result; //display the result in an HTML element
+    console.log(a);
+}, function(status) { //error detection....
+    alert('Something went wrong.');
 });
