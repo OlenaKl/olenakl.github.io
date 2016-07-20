9ae6b19068ad1eba1b768f704072f7a3854a57c0
Elena.Kulichenko <elena.kulichenko@devellar.com>
2016-07-20 11:00:37 +0300

    trying the github io

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
     // OR using $.ajax()
     $.ajax({
         type:     "GET",
-        url:      "https://www.instagram.com/justinbieber/media",
-        dataType: "json",
+        url:      "https://www.instagram.com/justinbieber/media?callback=?",
+        dataType: "jsonp",
         success: function(data){
             console.log(data);
         }
