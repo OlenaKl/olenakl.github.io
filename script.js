10aa282f1d8299ba4e1661fd8a9df0b329ed9be6
Elena.Kulichenko <elena.kulichenko@devellar.com>
2016-07-20 09:21:22 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Elena.Kulichenko on 19.07.2016.
+ */
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status == 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status);
+        }
+    };
+    xhr.send();
+};
+
+getJSON('http://www.instagram.com/justinbieber/media', function(err, data) {
+    if (err != null) {
+        alert('Something went wrong: ' + err);
+    } else {
+        alert('Your Json result is:  ' + data.result);
+        result.innerText = data.result;
+    }
+});
